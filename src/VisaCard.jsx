5c11e1d54f2af7762ba3654dfa277da5403d188f
ksa-app <sevenstar2202@gmail.com>
2026-03-31 14:58:06 +0600

    ok

--- src/VisaCard.jsx
+++ src/VisaCard.jsx
@@ -22,7 +22,7 @@ export default function VisaCard() {
         <div className="info">
           <div className="row">
             <div className="label">Visa No.</div>
-            <div className="value">6156048410</div>
+            <div className="value">6163672829</div>
           </div>
           <div className="row">
             <div className="label">Religion</div>
@@ -30,7 +30,7 @@ export default function VisaCard() {
           </div>
           <div className="row">
             <div className="label">Passport Expiry</div>
-            <div className="value">10/02/2034</div>
+            <div className="value">13/10/2035</div>
           </div>
           <div className="row">
             <div className="label">Nationality</div>
@@ -38,7 +38,7 @@ export default function VisaCard() {
           </div>
           <div className="row">
             <div className="label">Date of Birth</div>
-            <div className="value">08/11/1996</div>
+            <div className="value">10/10/2002</div>
           </div>
           <div className="row">
             <div className="label">Sex</div>
@@ -48,9 +48,9 @@ export default function VisaCard() {
       </div>
 
       <div className="mrz">
-        &lt;BGDJOYNUL ISLAM&lt;&lt;SHEKH MD&lt;&lt;&lt;&lt;&lt;&lt;&lt;&lt;&lt;&lt;&lt;&lt;
+        &lt;BGDHAQUE&lt;&lt;MD NAZMUL &lt;&lt;&lt;&lt;&lt;&lt;&lt;&lt;&lt;&lt;&lt;&lt;
         <br />
-        A14051028BGD15052000103092033&lt;&lt;&lt;&lt;&lt;&lt;&lt;&lt;02
+        A20526247BGD15052000103092033&lt;&lt;&lt;&lt;&lt;&lt;&lt;&lt;02
       </div>
 
       <div className="status">Valid</div>
